0d95c418bce2505e6733d09f7499cca4403e712d
narobinson1 <nicolas.alexander.robinson.2@gmail.com>
2024-01-10 16:56:38 +0100

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar as ExpoStatusBar } from "react-native"
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+} from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <   SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
