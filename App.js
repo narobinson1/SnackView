f5a49604c6ec37dae2ed004c21a4e955bad7954c
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-11 12:23:48 +0100

    custom fonts

--- App.js
+++ App.js
@@ -2,19 +2,28 @@ import { StatusBar as ExpoStatusBar } from "react-native";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { theme } from "./src/infrastructure/index";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-} from "react-native";
-
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
