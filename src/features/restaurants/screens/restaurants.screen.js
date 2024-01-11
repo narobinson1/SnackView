40ea90c73e0df0968fcf12831e7db3a208c6c9ce
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-11 11:06:26 +0100

    Add search bar

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+} from "react-native";
+
+export const RestaurantsScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <Text>list</Text>
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
