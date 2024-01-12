63a4d45d452104ef5f2882be1f76ddd9d781901b
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-12 15:10:20 +0100

    cart context

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { LiteCreditCardInput } from "react-native-credit-card-input";
+import { cardTokenRequest } from "../../../services/checkout/checkout.service";
 
-export const CreditCardInput = () => {
-  const onChange = (formData) => {
+export const CreditCardInput = ({ name = "Mo" }) => {
+  const onChange = async (formData) => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
-    console.log(isIncomplete);
+    const expiry = values.expiry.split("/");
+    const card = {
+      number: values.number.replace(/ /g, ""),
+      exp_month: values.expiry.split("/")[0],
+      exp_year: values.expiry.split("/")[1],
+      cvc: values.cvc,
+    };
+    const info = await cardTokenRequest(card);
   };
   return <LiteCreditCardInput onChange={onChange} />;
 };
