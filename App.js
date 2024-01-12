4fa34501c98a9d723bcc09d6a149638a35a8e7ea
narobinson1 <nicolas.alexander.robinson@gmail.com>
2024-01-12 11:26:01 +0100

    complete auth

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar as ExpoStatusBar } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import { Text } from "react-native";
 import {
@@ -9,7 +12,7 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
-import { theme } from "./src/infrastructure/index";
+import { theme } from "./src/infrastructure/theme/index";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
@@ -17,7 +20,20 @@ import { restaurantsRequest } from "./src/services/restaurants/restaurants.servi
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 import { Navigation } from "./src/infrastructure/navigation/index";
+import { AuthenticationContext } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfDYprIjCEhdt6ozbYem5glIo07R9pSS0",
+  authDomain: "snackview-dc76a.firebaseapp.com",
+  projectId: "snackview-dc76a",
+  storageBucket: "snackview-dc76a.appspot.com",
+  messagingSenderId: "890396719394",
+  appId: "1:890396719394:ios:a7f14a5d29c17638310d7b",
+};
+
+initializeApp(firebaseConfig);
 
 const TAB_ICON = {
   Restaurants: "md-restaurant",
@@ -53,13 +69,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View } from "react-native";
+import { Spacer } from "../../../components/utility/spacer.component";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  Title,
+} from "../components/account.styles";
+
+export const AccountScreen = ({ navigation }) => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>SnackView</Title>
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
+};
